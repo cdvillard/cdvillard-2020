801c0a9646b08ac6d8932154c5c3e268f2071ed2
Charles Villard <c.d.villard@gmail.com>
2020-04-04 14:28:51 -0400

    Componentize header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
+    <Header></Header>
     <slot />
+    <Footer></Footer>
   </div>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
-}
-</static-query>
+<script>
+import Header from "./../components/header";
+import Footer from "./../components/footer";
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
 
 <style lang="scss">
 body {
